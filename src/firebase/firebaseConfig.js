87024c853843ca9b16b798246f6a903c3e97fb3a
Sandewdi Nowanya <nowanyafonseka@gmail.com>
2025-02-12 17:24:27 +0530

    feat: Add Student Login Feature using Firebase

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,19 +3,18 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAgNQk250elb8rLxxiCDrz7CiTKNz0P74s",
-    authDomain: "spacefix-app.firebaseapp.com",
-    projectId: "spacefix-app",
-    storageBucket: "spacefix-app.firebasestorage.app",
-    messagingSenderId: "282490331387",
-    appId: "1:282490331387:web:4f306f6fc540122479505e",
-    measurementId: "G-RBBZZBN6D5"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const googleProvider = new GoogleAuthProvider();
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
 
-export { auth, db, googleProvider };
+export default app;
